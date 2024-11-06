68ad63bfe60799b53cb6e1f96a44a04a3180937b
Turbero <dicon18@gmail.com>
2024-11-06 10:09:42 +0100

    Renaming project

--- BetterSplitDialog/Properties/AssemblyInfo.cs
+++ BetterSplitDialog/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada con un ensamblado.
-[assembly: AssemblyTitle("QuantityPicker")]
+[assembly: AssemblyTitle("BetterSplitDialog")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("QuantityPicker")]
+[assembly: AssemblyProduct("BetterSplitDialog")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
